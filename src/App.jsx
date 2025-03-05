46b9f8b85cc29e5530edda652c7b11def94a8969
Damian Bednarz <damidbb@gmail.com>
2025-03-05 11:31:42 +0100

    changed to vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Form } from "~/components/Form";
+import {Form} from "../src/components/Form"
+export default function App() {
 
-export default function Home() {
   return (
     <main className="w-full h-full flex justify-center py-[96px]">
       <div className="w-[342px] sm:w-[434px] flex flex-col justify-start">
